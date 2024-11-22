3639eac985429ff607ff49eb983ff6ef0a0524ca
ayukifujimoto <ayukif03855675310kki@gmail.com>
2024-11-22 18:50:29 +0900

    entity

--- src/main/java/com/example/umigatari/model/account.java
+++ src/main/java/com/example/umigatari/model/account.java
@@ -1,5 +1,14 @@
 package com.example.umigatari.model;
 
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
 public class account {
-    
+    @Id
+    private Long id;
+    private String name;
+    private String password;
+    private int count;
+    private String mail;
 }
--- src/main/java/com/example/umigatari/model/quiz.java
+++ src/main/java/com/example/umigatari/model/quiz.java
@@ -1,5 +1,17 @@
 package com.example.umigatari.model;
 
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
 public class quiz {
+    @Id
+    private Long id;
+    private String question;
+    private String right;
+    private String otherone;
+    private String othertwo;
+    private int type;
+    private boolean check;
     
 }
