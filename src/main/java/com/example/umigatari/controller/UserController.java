bad1176b8918fff2c2923d5c94c7d85d5b5308c3
yoshizakiyuiri <ys03101409um@gmail.com>
2024-11-25 15:43:14 +0900

    コントローラー11/25

--- src/main/java/com/example/umigatari/controller/UserController.java
+++ src/main/java/com/example/umigatari/controller/UserController.java
@@ -10,55 +10,58 @@ public class UserController {
     private UserController userController;
 
     public String mail(){
-
+        return "createaccountone";
     }
 
     public String sendMail(){
-
+        return "createaccountone";
     }
 
     public String account(){
-
+        return "createaccounttwo";
     }
 
     public String createAccount(){
-
+        quizService.createAccount();
     }
 
     public String login(){
-
+        return "login";
     }
 
     public String loginPassword(){
-
+        quizService.readPassword();
     }
 
     public String passwordmail(){
-
+        return "forgotpassone";
     }
 
     public String readmail(){
-
+        quizService.readMail();
     }
 
     public String password(){
-
+        return "forgotpasstwo";
     }
 
     public String changePassword(){
-
+        quizService.updatePassword();
+        //同じか判定
     }
 
     public String deleteAccount(){
-
+        quizService.deleteAccount();
     }
 
     public String getStamp(){
-
+        return "stamp";
     }
 
     public String ranking(){
-        
+        quizService.myRanking();
+        quizService.rankingAccount();
+        //ページを表示
     }
     
 }
