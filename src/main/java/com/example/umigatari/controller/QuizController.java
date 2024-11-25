9c4486fe05d99a04b1dfebf0b5b57981bcd071c2
yoshizakiyuiri <ys03101409um@gmail.com>
2024-11-25 14:35:14 +0900

    コントローラー完了

--- src/main/java/com/example/umigatari/controller/QuizController.java
+++ src/main/java/com/example/umigatari/controller/QuizController.java
@@ -1,5 +1,68 @@
 package com.example.umigatari.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.example.umigatari.service.QuizService;
+
+@Controller
+
 public class QuizController {
+
+    @Autowired
+    private QuizService quizService;
+
+    public String randomThreeQuiz(){
+
+    }
+
+    public String showOneQuiz(){
+
+    }
+
+    public String check(){
+
+    }
+
+    public String createQuizPage(){
+
+    }
+
+    public String createQuiz(){
+
+    }
+
+    public String admin(){
+
+    }
+
+    public String adminCreatQuizPage(){
+
+    }
+
+    public String adminCreateQuiz(){
+
+    }
+
+    public String checkQuizPage(){
+
+    }
+
+    public String checkQuiz(){
+
+    }
+
+    public String quizListPage(){
+
+    }
+
+    public String deleteQuiz(){
+
+    }
+
+    public String updateQuiz(){
+        
+    }
+
     
 }
--- src/main/java/com/example/umigatari/controller/UserController.java
+++ src/main/java/com/example/umigatari/controller/UserController.java
@@ -1,5 +1,64 @@
 package com.example.umigatari.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+
 public class UserController {
+    @Autowired
+    private UserController userController;
+
+    public String mail(){
+
+    }
+
+    public String sendMail(){
+
+    }
+
+    public String account(){
+
+    }
+
+    public String createAccount(){
+
+    }
+
+    public String login(){
+
+    }
+
+    public String loginPassword(){
+
+    }
+
+    public String passwordmail(){
+
+    }
+
+    public String readmail(){
+
+    }
+
+    public String password(){
+
+    }
+
+    public String changePassword(){
+
+    }
+
+    public String deleteAccount(){
+
+    }
+
+    public String getStamp(){
+
+    }
+
+    public String ranking(){
+        
+    }
     
 }
