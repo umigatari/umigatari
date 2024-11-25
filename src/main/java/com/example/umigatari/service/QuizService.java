222ca0297ae6129b936fe050d74810ce58ff33be
yoshizakiyuiri <ys03101409um@gmail.com>
2024-11-25 11:58:17 +0900

    メソッド作った

--- src/main/java/com/example/umigatari/service/QuizService.java
+++ src/main/java/com/example/umigatari/service/QuizService.java
@@ -1,5 +1,42 @@
 package com.example.umigatari.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.umigatari.model.quiz;
+import com.example.umigatari.repository.QuizRepository;
+
+@Service
 public class QuizService {
+    @Autowired
+    private QuizRepository quizRepository;
+
+    public quiz readQuiz(){
+        
+    }
+
+    public quiz randomThreeQuiz(){
+
+    }
+
+    public void insertQuiz(){
+
+    }
+
+    public void deleteQuiz(){
+
+    }
+
+    public void updateQuiz(){
+
+    }
+
+    public quiz listQuiz(){
+
+    }
+
+    public quiz checkListQuiz(){
+        
+    }
     
 }
--- src/main/java/com/example/umigatari/service/UserService.java
+++ src/main/java/com/example/umigatari/service/UserService.java
@@ -1,5 +1,40 @@
 package com.example.umigatari.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+@service
+
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void createAccount(){
+
+    }
+
+    public account rankingAccount(){
+
+    }
+
+    public rank myRanking(){
+
+    }
+
+    public password readPassword(){
+
+    }
+
+    public void updatePassword(){
+
+    }
+
+    public void deleteAccount(){
+
+    }
+
+    public mail readMail(){
+        
+    }
+
     
 }
