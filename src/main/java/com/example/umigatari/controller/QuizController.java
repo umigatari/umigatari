6b3f0db9cd368f3b2fb5aba08ca1e540b33b4195
yoshizakiyuiri <ys03101409um@gmail.com>
2024-11-26 15:10:00 +0900

    マッピング

--- src/main/java/com/example/umigatari/controller/QuizController.java
+++ src/main/java/com/example/umigatari/controller/QuizController.java
@@ -2,6 +2,8 @@ package com.example.umigatari.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.umigatari.model.quiz;
 import com.example.umigatari.service.QuizService;
@@ -13,54 +15,67 @@ public class QuizController {
     @Autowired
     private QuizService quizService;
 
+    @PostMapping("")
     public String randomThreeQuiz(){
         quizService.randomThreeQuiz();
     }
 
+    @PostMapping("")
     public String showOneQuiz(){
         quizService.readQuiz();
     }
 
+    @PostMapping("")
     public String check(){
         return "check";
     }
 
+    @GetMapping("")
     public String createQuizPage(){
         return "adduserquiz";
     }
 
+    @PostMapping("")
     public String createQuiz(){
         quizService.insertQuiz();
     }
 
+    @GetMapping("")
     public String admin(){
         return "admin";
     }
 
+    @GetMapping("")
     public String adminCreatQuizPage(){
         return "addquiz";
     }
 
+    @PostMapping("")
     public String adminCreateQuiz(){
         quizService.insertQuiz();
     }
 
+    @GetMapping("")
     public String checkQuizPage(){
         quizService.checkListQuiz();
     }
 
+    @PostMapping("")
     public String checkQuiz(){
         quizService.updateQuiz();
     }
 
+    @GetMapping("")
     public String quizListPage(){
         quizService.listQuiz();
     }
 
+    @PostMapping("")
     public String deleteQuiz(){
         quizService.deleteQuiz();
     }
 
+    @PostMapping("")
     public String updateQuiz(){
         quizService.updateQuiz();
     }
