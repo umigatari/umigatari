70326bdf31005d4d70490cf503f67b9b8911fa00
yoshizakiyuiri <ys03101409um@gmail.com>
2024-11-26 14:32:17 +0900

    コメント記載

--- src/main/java/com/example/umigatari/repository/QuizRepository.java
+++ src/main/java/com/example/umigatari/repository/QuizRepository.java
@@ -14,38 +14,44 @@ public class QuizRepository {
     
     @Autowired
     public JdbcTemplate jdbcTemplate;
-
+    
+    //ランダムにクイズを3つ表示する
     public List<quiz> randomThreeQuiz() {
         String sql = "SELECT * FROM quiz ORDER BY RANDOM() LIMIT 3";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(quiz.class));
     }
 
+    //クイズを参照する
     public quiz readQuiz(Long id) {
         String sql = "SELECT * FROM quiz WHERE id = ?";
         return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(quiz.class), id);
 }
 
-
+    //追加する
     public void insertQuiz(quiz quiz){
         String sql = "insert into quiz (question,correct,other_one,other_two,type,confirmation) values (?, ?, ?, ?, ?, ?)";
         jdbcTemplate.update(sql,quiz.getQuestion(),quiz.getCorrect(),quiz.getOtherone(),quiz.getOthertwo(),quiz.getType(),quiz.isConfirmation());
     }
 
+    //削除する
     public void deleteQuiz(Long id){
         String sql = "DELETE FROM quiz WHERE id = ?";
         jdbcTemplate.update(sql, id);
     }
 
+    //更新する
     public void updateQuiz(quiz quiz){
         String sql = "UPDATE quizzes SET question = ?, correct = ?, otherone = ?,othertwo = ?,type = ?,confirmation = ? WHERE id = ?";
         jdbcTemplate.update(sql, quiz.getQuestion(), quiz.getCorrect(), quiz.getOtherone(), quiz.getOthertwo(),quiz.getType(),quiz.isConfirmation(),quiz.getId());
     }
 
+    //一覧を表示する
     public List<quiz> listQuiz(){
     String sql = "SELECT * FROM quiz";
     return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(quiz.class));
     }
 
+    //チェックが必要な一覧を表示する
     public List<quiz> checkListQuiz() {
         String sql = "SELECT * FROM quiz WHERE  confirmation = TRUE";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(quiz.class));
--- src/main/java/com/example/umigatari/repository/UserRepository.java
+++ src/main/java/com/example/umigatari/repository/UserRepository.java
@@ -13,11 +13,13 @@ public class UserRepository {
     @Autowired
     public JdbcTemplate jdbcTemplate;
 
+    //アカウントをつくる
     public void createAccount(account account){
         String sql = "INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)";
         jdbcTemplate.update(sql, account.getName(), account.getPassword(), account.getMail());
     }
 
+    //正答数の多い順から取り出す
     public List<account> rankingAccount(int limit){
         String sql = "SELECT name,count FROM accounts ORDER BY count DESC LIMIT ?";
         return jdbcTemplate.query(sql, (rs, rowNum) -> {
@@ -28,27 +30,31 @@ public class UserRepository {
         }, limit);
     }
 
+    //順位を表示する
     public int myRanking(Long id) {
         String sql = "SELECT rank FROM (SELECT ID, ROW_NUMBER() OVER (ORDER BY count DESC) AS rank FROM テーブル名) AS RankedData WHERE ID = ?";
         return jdbcTemplate.queryForObject(sql, Integer.class, id);
     }
     
-
+    //名前に対応するパスワードを表示する
     public String readPassword(String name){
         String sql ="SELECT password FROM account WHERE username = ?";
         return jdbcTemplate.queryForObject(sql,String.class,name);
     }
 
+    //名前に対応するパスワードを変更する
     public void updatePassword(String name,String password){
         String sql = "UPDATE account SET password = ? WHERE name = ?";
         jdbcTemplate.update(sql,password, name);
     }
 
+    //idに対応するアカウントを削除する
     public void deleteAccount(Long id){
         String sql = "DELETE FROM account WHERE id = ?";
         jdbcTemplate.update(sql, id);      
     }
 
+    //名前に対応するメールを取得する
     public String readMail(String name){
         String sql = "SELECT mail FROM account WHERE username = ?";
         return jdbcTemplate.queryForObject(sql,String.class,name);      
