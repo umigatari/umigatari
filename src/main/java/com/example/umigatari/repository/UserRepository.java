9229537deaa6b51bf6fb8156a1fd72ec2abc0588
ayukifujimoto <ayukif03855675310kki@gmail.com>
2024-11-28 11:49:46 +0900

    ログイン機能はまだ。クイズ表示、管理者機能はほぼ完成。あとは調整。

--- src/main/java/com/example/umigatari/repository/UserRepository.java
+++ src/main/java/com/example/umigatari/repository/UserRepository.java
@@ -15,13 +15,13 @@ public class UserRepository {
 
     //アカウントをつくる
     public void createAccount(account account){
-        String sql = "INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO accounts (name, password, mail) VALUES (?, ?, ?)";
         jdbcTemplate.update(sql, account.getName(), account.getPassword(), account.getMail());
     }
 
     //正答数の多い順から取り出す
     public List<account> rankingAccount(int limit){
-        String sql = "SELECT name,count FROM accounts ORDER BY count DESC LIMIT ?";
+        String sql = "SELECT name,count FROM account ORDER BY count DESC LIMIT ?";
         return jdbcTemplate.query(sql, (rs, rowNum) -> {
             account account = new account();
             account.setName(rs.getString("name"));
@@ -32,13 +32,13 @@ public class UserRepository {
 
     //順位を表示する
     public int myRanking(Long id) {
-        String sql = "SELECT rank FROM (SELECT ID, ROW_NUMBER() OVER (ORDER BY count DESC) AS rank FROM テーブル名) AS RankedData WHERE ID = ?";
+        String sql = "SELECT rank FROM (SELECT ID, ROW_NUMBER() OVER (ORDER BY count DESC) AS rank FROM account) AS RankedData WHERE id = ?";
         return jdbcTemplate.queryForObject(sql, Integer.class, id);
     }
     
     //名前に対応するパスワードを表示する
     public String readPassword(String name){
-        String sql ="SELECT password FROM account WHERE username = ?";
+        String sql ="SELECT password FROM account WHERE name = ?";
         return jdbcTemplate.queryForObject(sql,String.class,name);
     }
 
@@ -56,7 +56,19 @@ public class UserRepository {
 
     //名前に対応するメールを取得する
     public String readMail(String name){
-        String sql = "SELECT mail FROM account WHERE username = ?";
+        String sql = "SELECT mail FROM account WHERE name = ?";
         return jdbcTemplate.queryForObject(sql,String.class,name);      
+    }
+
+    //カウントに+１する
+    public void countUp(Long id){
+        String sql = "update account set count = count + 1 where id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    //nameに対するIdを表示する
+    public Long redId(String name){
+        String sql ="select id from account where name = ?";
+        return jdbcTemplate.queryForObject(sql,Long.class,name);
     }
 }
